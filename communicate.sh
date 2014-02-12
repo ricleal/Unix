39df5b51fe76b77057311e2f576863f3d56aecbf
Ricardo Ferraz Leal <ricardo.leal@ill.eu>
2014-02-12 17:47:14 +0100

    Added Lamp

--- communicate.sh
+++ communicate.sh
@@ -1,21 +1,29 @@
 #!/bin/sh
 # This is a comment
 
-executable="/home/leal/git/Unix/Debug/Unix"
-named_pipe=`mktemp -u`
+executable="/net/serhom/home/cs/richard/Free_Lamp81/START_lamp -nws"
+
+in_pipe=`mktemp -u`
+out_pipe=`mktemp`
 
 echo "Communicating with process ${executable} with pid ${process_pid}."
 
 # Exec
+mkfifo ${in_pipe}
+${executable} < ${in_pipe} > ${out_pipe} &
+touch ${in_pipe}
 
-mkfifo ${named_pipe}
-${executable} < ${named_pipe} &
 #process_pid=`pgrep ${executable}`
 process_pid=$!
 
 
-echo "test 1" > ${named_pipe}
-echo "test 2" > ${named_pipe}
+echo "Executable running with pid ${process_pid}. Use ${in_pipe} for communication."
 
-kill -INT ${process_pid}
-rm ${named_pipe}
+
+tail -f ${out_pipe}
+
+# in other term
+echo "print,'Hello world'" > ${in_pipe}
+
+#kill -INT ${process_pid}
+#rm ${named_pipe}
